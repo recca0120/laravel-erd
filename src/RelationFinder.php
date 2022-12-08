695e207dbe40dd0f9e49fd6677b8d6fc9d071f6d
recca0120 <recca0120@gmail.com>
2022-12-09 03:43:37 +0800

    find out belongs to many

--- src/RelationFinder.php
+++ src/RelationFinder.php
@@ -3,6 +3,8 @@
 namespace Recca0120\LaravelErdGo;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\MorphToMany;
 use Illuminate\Database\Eloquent\Relations\Relation;
 use Illuminate\Support\Collection;
 use ReflectionClass;
@@ -36,6 +38,27 @@ class RelationFinder
                 return null;
             }
 
+            if ($return instanceof MorphToMany) {
+                dump($return->getMorphType());
+                dump($return->getMorphClass());
+            }
+            if ($return instanceof BelongsToMany) {
+                dump($return->getExistenceCompareKey());
+                dump($return->getForeignPivotKeyName());
+                dump((new ReflectionClass($return->getRelated()))->getName());
+                dump($return->getQualifiedForeignPivotKeyName());
+                dump($return->getRelatedPivotKeyName());
+                dump($return->getQualifiedRelatedPivotKeyName());
+                dump($return->getParentKeyName());
+                dump($return->getQualifiedParentKeyName());
+                dump($return->getRelatedKeyName());
+                dump($return->getQualifiedRelatedKeyName());
+                dump($return->getRelationName());
+                dump($return->getPivotAccessor());
+                dump($return->getPivotColumns());
+                return;
+            }
+
             $relationType = (new ReflectionClass($return))->getShortName();
             $modelName = (new ReflectionClass($return->getRelated()))->getName();
 
@@ -51,6 +74,8 @@ class RelationFinder
                 ]
             ];
         } catch (Throwable $e) {
+            dump($method->getName());
+            dump($e->getMessage());
         }
 
         return null;
