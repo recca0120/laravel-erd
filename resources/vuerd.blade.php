e0a59ea1dfb53e10fb742f8e6f9cb866f2febd0d
recca0120 <recca0120@gmail.com>
2022-12-14 14:32:45 +0800

    add views

---
+++ resources/vuerd.blade.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <style type="text/css">
+        body {
+            margin: 0;
+            height: 100vh;
+        }
+    </style>
+</head>
+<body>
+<erd-editor automatic-layout></erd-editor>
+<script src="https://cdn.jsdelivr.net/npm/vuerd/dist/vuerd.min.js"></script>
+<script>
+    const editor = document.querySelector('erd-editor');
+    editor.loadSQLDDL();
+</script>
+</body>
+</html>
