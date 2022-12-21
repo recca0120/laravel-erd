b63a1276727d418a01a9932fc88cdbb240a7aaf6
recca0120 <recca0120@gmail.com>
2022-12-21 12:55:59 +0800

    rename show

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,6 @@ use Illuminate\Support\Facades\Route;
 use Recca0120\LaravelErd\Http\Controllers\LaravelErdController;
 
 Route::get(config('laravel-erd.uri') . '/{file?}', [LaravelErdController::class, 'index'])
-    ->name('laravel-erd.index')
+    ->name('laravel-erd.show')
     ->middleware(config('laravel-erd.middleware'))
     ->where('file', '.*');
